b29cf0ff5dba8511cdb9848b864d53c7509515be
Markus Kauppila <markus.kauppila@gmail.com>
2012-09-25 20:28:58 +0300

    Add main to the script.

--- lj_handler.py
+++ lj_handler.py
@@ -1,13 +1,31 @@
 #! /usr/bin/python
 
 """
+Handles downloading and emailing the latest issue of Linux Journal.
+
+Put this script as cron task.
+
+Usage:
+	AN=XXXXXX MAIL_TO=markus.kauppila@gmail.com ./lj_handler.py
+		AN= Linux Journal account number
+		MAIL_TO= your.email@address.com
+
+	./lj_handler --an XXXXXX --download_all --mail_to markus.kauppila@gmail.com
+				 --filename "Linux Journal" --format=[pdf,epub,mobi]
+
+	- Two modes:	
+		- download all
+		- mail the latest issue
+
+TODO:
+	- emailing
+	- Add proper support for different file formats
 """
 
 import os
 import urllib2 
 import HTMLParser
-
-download_links = []
+import urlparse
 
 class LinkParser(HTMLParser.HTMLParser):
 	def __init__(self):
@@ -24,29 +42,81 @@ class LinkParser(HTMLParser.HTMLParser):
 			link = attrs[0][1]
 			self.verify_link(link)
 
-# users account number for linux journal subscription
-# TODO(mk): print usage and exit if AN is missing
-account_number = os.environ['AN']
 
-# download url
-download_url = 'https://secure2.linuxjournal.com/pdf/dljdownload.php?ucLJFooter_accountnumber='
+def download_issue(issue_info):
+	""" Download the issue 
+	"""
+	issue_number, file_format, link = issue_info
+	filename = "LinuxJournal-" + issue_number + ".pdf"
+	with open(filename, 'w') as file:
+		pdf_data = pdf.read()
+		file.write(pdf_data)
+	return filename
+	
 
-full_url = '%s%s' % (download_url, account_number)
-response = urllib2.urlopen(full_url)
-page = response.read()
+if __name__ == "__main__":
+	# users account number for linux journal subscription
+	# TODO(mk): print usage and exit if AN is missing
+	account_number = os.environ['AN']
 
-parser = LinkParser()
-parser.feed(page)
+	download_url = 'https://secure2.linuxjournal.com/pdf/dljdownload.php?ucLJFooter_accountnumber='
 
-for link in download_links:
-	print link
+	# Retrieve download page
+	full_url = '%s%s' % (download_url, account_number)
+	response = urllib2.urlopen(full_url)
+	page = response.read()
 
-# parse issue number
+	# parse the download links
+	parser = LinkParser()
+	parser.feed(page)
 
-print "downloading magazine'
+	latest_issue_number = None
+	# remember the test without the file
+	with open('latest') as memory:
+		latest_issue_number = memory.readline()
+		print "latest issue: %s" % latest_issue_number
 
-download_link = "%s&action=spit" % download_links[0]
-pdf = urllib2.urlopen(download_link)
-with open('filename.pdf', 'w') as file:
-	file.write(pdf.read())
+
+	issue_information = []
+
+	for link in parser.verified_links:
+		# parse issue number
+		parsed_url = urlparse.urlparse(link)
+		query = urlparse.parse_qs(parsed_url.query)
+
+		tcode = query['tcode']
+		codes = tcode[0].split('-')
+
+		file_format = codes[0]
+		issue_number = codes[1]
+		link += '&action=spit'
+
+		issue_information.append((issue_number, file_format, link))
+
+		#print "Linux Journal - %s as %s" % (issue_number, file_format)
+
+
+	if True:
+		if issue_number > latest_issue_number and False:
+			print "downloading magazine"
+
+			# modify the link for direct download
+			link += '&action=spit'
+			pdf = urllib2.urlopen(link)
+			filename = "LinuxJournal-" + issue_number + ".pdf"
+			with open(filename, 'w') as file:
+				pdf_data = pdf.read()
+				file.write(pdf_data)
+
+			# update the latest issue
+			with open('latest', 'w') as memory:
+				memory.write(issue_number)
+		else:
+			pass
+			#print "No new issue available"
+	else:
+		# download all mode, download and safe
+		for issue in issue_information:
+			print issue
+
 
