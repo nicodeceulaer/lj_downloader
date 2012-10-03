730c1cae739bd57363f2e5b662e175a0a551ac97
Markus Kauppila <markus.kauppila@gmail.com>
2012-10-03 22:01:16 +0300

    Add MIT license

--- lj_downloader.py
+++ lj_downloader.py
@@ -1,19 +1,27 @@
 #! /usr/bin/python
-
 """
-Handles downloading and emailing the latest issue of Linux Journal.
+Copyright (c) 2012 Markus Kauppila <markus.kauppila@gmail.com>
 
-Put this script as cron task.
+Permission is hereby granted, free of charge, to any person obtaining 
+a copy of this software and associated documentation files (the 
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to 
+permit persons to whom the Software is furnished to do so, subject to 
+the following conditions:
 
-Usage:
-    AN=XXXXXX MAIL_TO=markus.kauppila@gmail.com ./lj_handler.py
-        AN= Linux Journal account number
-        MAIL_TO= your.email@address.com
+The above copyright notice and this permission notice shall be 
+included in all copies or substantial portions of the Software.
 
-    ./lj_handler --an XXXXXX --download_all --mail_to markus.kauppila@gmail.com
-                 --filename "Linux Journal" --format=[pdf,epub,mobi]
-                 --dst-directory 'l'
-
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+"""
+"""
 TODO:
     - handle incorrect account number
     - put latest to home directory and make it hidden
